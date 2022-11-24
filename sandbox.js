582965abe92969dec90794b6f4952d9508997ab7
Adit Tanu <adiettanu27@gmail.com>
2022-11-24 13:10:19 +0700

    normal types

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,39 @@
 var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+// names = 'names';
+names.push('toad');
+// names.push(3);
+// names[0] = 3
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun')
+var mixed = ['ken', 4, 'chun-li', 8, 9, true];
+mixed.push(34);
+mixed.push('ryu');
+mixed[0] = 3;
+// objects 
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+    name: 'yoshi',
+    belt: 'black',
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,53 @@
-const character = 'mario';
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-console.log(character);
 
-const inputs = document.querySelectorAll('input');
+// character = 20;
+character = 'luigi';
+age = 40;
+isBlackBelt = true
 
-inputs.forEach(input => {
-  console.log(input);
-});
+
+const circ = (diameter: number) => { 
+  return diameter * Math.PI
+}
+
+console.log(circ(7.5));
+
+
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+// names = 'names';
+
+names.push('toad');
+// names.push(3);
+// names[0] = 3
+let numbers = [10, 20, 30, 40];
+numbers.push(25)
+// numbers.push('shaun')
+
+let mixed = ['ken', 4, 'chun-li', 8, 9, true];
+
+mixed.push(34);
+mixed.push('ryu');
+mixed[0] = 3;
+
+// objects 
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age : 30
+}
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+  name : 'yoshi',
+  belt : 'black',
+  age : 40
+}
