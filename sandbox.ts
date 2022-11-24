2382aa2162f13bbb8a617245db0ebab4fb917954
Adit Tanu <adiettanu27@gmail.com>
2022-11-24 13:17:00 +0700

    dynamic type / any

--- sandbox.ts
+++ sandbox.ts
@@ -1,53 +1,25 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+let age: any = 25;
 
-
-// character = 20;
-character = 'luigi';
-age = 40;
-isBlackBelt = true
-
-
-const circ = (diameter: number) => { 
-  return diameter * Math.PI
+age = true;
+console.log(age)
+age = 'hello';
+console.log(age)
+age = {
+  name : 'luigi'
 }
+console.log(age)
 
-console.log(circ(7.5));
+
+let mixed: any[] = [];
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false)
+console.log(mixed);
 
 
-// arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let ninja : { name: any, age: any};
 
-// names = 'names';
+ninja = {name : 'yoshi', age: 25}
+console.log(ninja)
 
-names.push('toad');
-// names.push(3);
-// names[0] = 3
-let numbers = [10, 20, 30, 40];
-numbers.push(25)
-// numbers.push('shaun')
-
-let mixed = ['ken', 4, 'chun-li', 8, 9, true];
-
-mixed.push(34);
-mixed.push('ryu');
-mixed[0] = 3;
-
-// objects 
-let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age : 30
-}
-
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking'];
-
-ninja = {
-  name : 'yoshi',
-  belt : 'black',
-  age : 40
-}
+ninja = {name :25, age : 'yoshi'}
